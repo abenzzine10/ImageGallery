ae01772b023da5480c1ff7b9c23db11cf70b1e97
abenzzine10 <54524081+abenzzine10@users.noreply.github.com>
2021-03-18 14:12:18 -0500

    Added Files from School Account

---
+++ index.js
@@ -0,0 +1,219 @@
+$("#img1").on( "click", function() {
+  $("#gallery").hide();
+  $("#image1").show();
+});
+
+$("#next1").on( "click", function() {
+  $("#image1").hide();
+  $("#image2").show();
+});
+
+$("#img2").on( "click", function() {
+  $("#gallery").hide();
+  $("#image2").show();
+});
+
+$("#previous2").on( "click", function() {
+  $("#image2").hide();
+  $("#image1").show();
+});
+
+$("#next2").on( "click", function() {
+  $("#image2").hide();
+  $("#image3").show();
+});
+
+$("#img3").on( "click", function() {
+  $("#gallery").hide();
+  $("#image3").show();
+});
+
+$("#previous3").on( "click", function() {
+  $("#image3").hide();
+  $("#image2").show();
+});
+
+$("#next3").on( "click", function() {
+  $("#image3").hide();
+  $("#image4").show();
+});
+
+$("#img4").on( "click", function() {
+  $("#gallery").hide();
+  $("#image4").show();
+});
+
+$("#previous4").on( "click", function() {
+  $("#image4").hide();
+  $("#image3").show();
+});
+
+$("#next4").on( "click", function() {
+  $("#image4").hide();
+  $("#image5").show();
+});
+
+$("#img5").on( "click", function() {
+  $("#gallery").hide();
+  $("#image5").show();
+});
+
+$("#previous5").on( "click", function() {
+  $("#image5").hide();
+  $("#image4").show();
+});
+
+$("#next5").on( "click", function() {
+  $("#image5").hide();
+  $("#image6").show();
+});
+
+$("#img6").on( "click", function() {
+  $("#gallery").hide();
+  $("#image6").show();
+});
+
+$("#previous6").on( "click", function() {
+  $("#image6").hide();
+  $("#image5").show();
+});
+
+$("#next6").on( "click", function() {
+  $("#image6").hide();
+  $("#image7").show();
+});
+
+$("#img7").on( "click", function() {
+  $("#gallery").hide();
+  $("#image7").show();
+});
+
+$("#previous7").on( "click", function() {
+  $("#image7").hide();
+  $("#image6").show();
+});
+
+$("#next7").on( "click", function() {
+  $("#image7").hide();
+  $("#image8").show();
+});
+
+$("#img8").on( "click", function() {
+  $("#gallery").hide();
+  $("#image8").show();
+});
+
+$("#previous8").on( "click", function() {
+  $("#image8").hide();
+  $("#image7").show();
+});
+
+$("#next8").on( "click", function() {
+  $("#image8").hide();
+  $("#image9").show();
+});
+
+$("#img9").on( "click", function() {
+  $("#gallery").hide();
+  $("#image9").show();
+});
+
+$("#previous9").on( "click", function() {
+  $("#image9").hide();
+  $("#image8").show();
+});
+
+$("#next9").on( "click", function() {
+  $("#image9").hide();
+  $("#image10").show();
+});
+
+$("#img10").on( "click", function() {
+  $("#gallery").hide();
+  $("#image10").show();
+});
+
+$("#previous10").on( "click", function() {
+  $("#image10").hide();
+  $("#image9").show();
+});
+
+$("#next10").on( "click", function() {
+  $("#image10").hide();
+  $("#image11").show();
+});
+
+$("#img11").on( "click", function() {
+  $("#gallery").hide();
+  $("#image11").show();
+});
+
+$("#previous11").on( "click", function() {
+  $("#image11").hide();
+  $("#image10").show();
+});
+
+$("#next11").on( "click", function() {
+  $("#image11").hide();
+  $("#image12").show();
+});
+
+$("#img12").on( "click", function() {
+  $("#gallery").hide();
+  $("#image12").show();
+});
+
+$("#previous12").on( "click", function() {
+  $("#image12").hide();
+  $("#image11").show();
+});
+
+$("#next12").on( "click", function() {
+  $("#image12").hide();
+  $("#image13").show();
+});
+
+$("#img13").on( "click", function() {
+  $("#gallery").hide();
+  $("#image13").show();
+});
+
+$("#previous13").on( "click", function() {
+  $("#image13").hide();
+  $("#image12").show();
+});
+
+$("#next13").on( "click", function() {
+  $("#image13").hide();
+  $("#image14").show();
+});
+
+$("#img14").on( "click", function() {
+  $("#gallery").hide();
+  $("#image14").show();
+});
+
+$("#previous14").on( "click", function() {
+  $("#image14").hide();
+  $("#image13").show();
+});
+
+$("#next14").on( "click", function() {
+  $("#image14").hide();
+  $("#image0").show();
+});
+
+$("#img0").on( "click", function() {
+  $("#gallery").hide();
+  $("#image0").show();
+});
+
+$("#previous0").on( "click", function() {
+  $("#image0").hide();
+  $("#image14").show();
+});
+
+$(".close").on( "click", function() {
+  $("#gallery").show();
+  $(".image").hide();
+});
